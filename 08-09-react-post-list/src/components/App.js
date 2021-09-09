92f19fc3fbfe468e0819a38ab2af80068ab742ef
Maksym Kostenko <kostenko.m.t@gmail.com>
2021-09-09 17:08:59 +0200

    Update App.js

--- 08-09-react-post-list/src/components/App.js
+++ 08-09-react-post-list/src/components/App.js
@@ -8,27 +8,21 @@ import PostList from "./posts/PostList"
 function App() {
     const [posts, setPosts] = useState(Store())
 
-    const setLike = id => {
+    const setSmth = (id, key) => {
         const postsArray = [...posts]
         const index = postsArray.findIndex(item => item.id === id)
         const post = {...postsArray[index]}
-        if (post.likes !== undefined)
-            post.likes++
-        else post.likes = 1
+        if (post[key] !== undefined)
+            post[key]++
+        else post[key] = 1
         postsArray[index] = post
         setPosts(postsArray)
     }
 
-    const setDislike = id => {
-        const postsArray = [...posts]
-        const index = postsArray.findIndex(item => item.id === id)
-        const post = {...postsArray[index]}
-        if (post.dislikes !== undefined)
-            post.dislikes++
-        else post.dislikes = 1
-        postsArray[index] = post
-        setPosts(postsArray)
-    }
+    const setLike = id => setSmth(id, 'likes')
+
+    const setDislike = id => setSmth(id, 'dislikes')
+
 
     return (
         <div className="App">
