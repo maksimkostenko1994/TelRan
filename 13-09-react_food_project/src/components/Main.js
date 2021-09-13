09705fb5f654b5d673a7c23f3c065ed60b2d27a2
Maksym Kostenko <kostenko.m.t@gmail.com>
2021-09-13 21:02:21 +0200

    Update food project
    Change Main component from "class" type to "function" type;

---
+++ 13-09-react_food_project/src/components/Main.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import getData from '../store/store';
+import MealList from './MealList';
+import MealWithComments from './MealWithComments';
+
+export const MealContext = React.createContext();
+
+const Main = () => {
+    const [meals, setMeals] = useState(getMealsFromJson())
+    const [currentMeal, setCurrentMeal] = useState(null)
+
+    const changeCurrentMeal = (id) => {
+        const newMeals = [...meals]
+        const index = newMeals.findIndex(meal => meal.idMeal === id)
+        const meal = {...newMeals[index]};
+        setCurrentMeal(meal)
+    }
+
+    const showMeals = () => {
+        setCurrentMeal(null)
+    }
+    const addComment = (id, comment) => {
+        const newMeals = [...meals]
+        const index = newMeals.findIndex(meal => meal.idMeal === id)
+        const meal = {...newMeals[index]};
+        const newComments = [...meal.comments];
+        newComments.push({...comment, id: Date.now(), note: +comment.note})
+        meal.comments = newComments;
+        meal.rate = (newComments.reduce((sum, comment) => sum += comment.note, 0) / newComments.length).toFixed(2);
+        newMeals[index] = meal;
+        setMeals(newMeals)
+        setCurrentMeal(meal)
+    }
+
+    return (
+        <MealContext.Provider value={{
+            changeCurrentMeal: changeCurrentMeal,
+            addComment: addComment
+        }}>
+            <div className='container'>
+                {currentMeal ?
+                    <MealWithComments meal={currentMeal}
+                                      showMeals={showMeals}/> :
+                    <MealList meals={meals}/>
+                }
+            </div>
+        </MealContext.Provider>
+    )
+
+}
+
+function getMealsFromJson() {
+    const meals = JSON.parse(getData()).meals;
+    meals.forEach(el => {
+        el.comments = [];
+        el.rate = 0
+    })
+    return meals
+}
+
+export default Main
+
